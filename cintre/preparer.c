b3d4542d7b10778e671fcde6aac20cf6de62f818
Grenier Celestin <celestin.grenier@hotmail.com>
2024-05-10 18:48:54 +0200

    wip
    + fill in the namespace
    ! may need to change approach for testing

--- cintre/preparer.c
+++ cintre/preparer.c
@@ -28,12 +28,13 @@ int indent = 0;
 #define indented(...)  for (bool _once = (++indent, emitln(__VA_ARGS__), true); _once; _once = (--indent, fprintf(result, "\n%*s", indent*4, ""), false))
 #define emit(...)  (EMIT_HERE, fprintf(result, __VA_ARGS__))
 #define emitln(...)  (emit(__VA_ARGS__), fprintf(result, "\n%*s", indent*4, ""))
+#define emit_empty()  fputc('\n', result)
 
 struct {
     dyarr(struct seen_fun { bufsl name; }) funs;
     dyarr(struct seen_tag { bufsl name; }) tags;
     dyarr(struct seen_tdf { bufsl name; }) tdfs;
-    dyarr(struct seen_var { bufsl name; }) objs;
+    dyarr(struct seen_obj { bufsl name; }) objs;
 } seen = {0};
 
 #define errdie(...) (report_lex_locate(&ls, "preparer: " __VA_ARGS__), exit(EXIT_FAILURE))
@@ -69,13 +70,25 @@ void emit_forward(struct decl_type cref ty, bufsl cref name)
     case KIND_STRUCT: emit("struct "); if (0)
     case KIND_UNION:  emit("union ");
         if (ty->name.len) emit("%.*s", bufmt(ty->name));
-        if (-1ul == ty->info.comp.count /* TODO: || comp_already_def(..) */) break;
+        if (-1ul == ty->info.comp.count) break;
+
+        bool found = false;
+        search_namespace (ty->name, seen.tags) {
+            found = true;
+            break;
+        }
+        if (found) break;
+
         indented (" {") for_linked (ty->info,comp) {
             emit_forward(&curr->decl->type, &curr->decl->name);
             if (curr->next) emitln(";");
             else emit(";");
         }
         emit("}");
+
+        struct seen_tag ref tag = dyarr_push(&seen.tags);
+        if (!tag) errdie("OOM");
+        tag->name = ty->name;
         break;
 
     case KIND_ENUM:
@@ -112,7 +125,7 @@ void emit_forward(struct decl_type cref ty, bufsl cref name)
         return; // name already done
     }
 
-    if (name) emit(" %.*s", bufmt(*name));
+    if (name && name->len) emit(" %.*s", bufmt(*name));
 }
 
 /// emit nested named struct/union that are defined, depth first, ie define the
@@ -145,13 +158,15 @@ void emit_named_comps_adpt_type_def(struct decl_type cref ty)
         indented (".info.comp= {") {
             size_t count = 0;
             indented (".fields= (struct adpt_comp_field[]){") for_linked (ty->info,comp) {
-                emitln("{ .name= \"%.*s\"", bufmt(curr->decl->name));
-                emitln(", .offset= offsetof(%s %.*s, %.*s)", lo_kind, bufmt(name), bufmt(curr->decl->name));
-                emit(", .type= &");
-                emit_adpt_type_val(&curr->decl->type, false);
-                if (curr->next) emitln(" },");
-                else emit(" },");
-                count++;
+                indented ("[%zu]= {", count++) {
+                    emitln(".name= \"%.*s\",", bufmt(curr->decl->name));
+                    emit(".type= &");
+                    emit_adpt_type_val(&curr->decl->type, false);
+                    emitln(",");
+                    emit(".offset= offsetof(%s %.*s, %.*s),", lo_kind, bufmt(name), bufmt(curr->decl->name));
+                }
+                if (curr->next) emitln("},");
+                else emit("},");
             }
             emitln("},");
             emit(".count= %zu,", count);
@@ -235,12 +250,14 @@ void emit_adpt_type_val(struct decl_type cref ty, bool const in_decl)
                 if (-1ul == ty->info.fun.count || 0 == ty->info.fun.count) emitln("NULL,");
                 else {
                     indented ("(struct adpt_fun_param[]){") for_linked (ty->info,fun) {
-                        emitln("{ .name= \"%.*s\"", bufmt(curr->decl->name));
-                        emit(", .type= &");
-                        emit_adpt_type_val(&curr->decl->type, false);
-                        if (curr->next) emitln(" },");
-                        else emit(" },");
-                        count++;
+                        indented ("[%zu]= {", count++) {
+                            emitln(".name= \"%.*s\",", bufmt(curr->decl->name));
+                            emit(".type= &");
+                            emit_adpt_type_val(&curr->decl->type, false);
+                            emit(",");
+                        }
+                        if (curr->next) emitln("},");
+                        else emit("},");
                     }
                     emitln("},");
                 }
@@ -273,6 +290,13 @@ void emit_extern(declaration cref decl)
     }
 
     if (KIND_FUN == decl->type.kind) {
+        bool found = false;
+        search_namespace (decl->name, seen.funs) {
+            found = true;
+            break;
+        }
+        if (found) return;
+
         emit("void %.*s_adapt_call(char* ret, char** args)", bufmt(decl->name));
         indented ("{") {
             if (0 == decl->type.info.fun.count) emitln("(void)args;");
@@ -293,6 +317,25 @@ void emit_extern(declaration cref decl)
             emit(");");
         }
         emitln("}");
+
+        struct seen_fun ref fun = dyarr_push(&seen.funs);
+        if (!fun) errdie("OOM");
+        fun->name = decl->name;
+    }
+
+    else {
+        bool found = false;
+        search_namespace (decl->name, seen.objs) {
+            found = true;
+            break;
+        }
+        if (found) return;
+
+        emitln("// object not quite handled anywhere yet");
+
+        struct seen_obj ref obj = dyarr_push(&seen.objs);
+        if (!obj) errdie("OOM");
+        obj->name = decl->name;
     }
 
     emit_named_comps_adpt_type_def(&decl->type.info.fun.ret->type);
@@ -319,6 +362,10 @@ void emit_typedef(declaration cref decl)
     else emitln(";");
 
     emitln("#define %.*s_adapt_type %.*s_adapt_tdf_type", bufmt(decl->name), bufmt(decl->name));
+
+    struct seen_tdf ref tdf = dyarr_push(&seen.tdfs);
+    if (!tdf) errdie("OOM");
+    tdf->name = decl->name;
 }
 // }}}
 
@@ -337,12 +384,12 @@ void emit_top(void* _, declaration cref decl, bufsl ref tok)
     case SPEC_EXTERN: // external linkage
     case SPEC_NONE: // default at file scope is external linkage
         emit_extern(decl);
-        fputc('\n', result);
+        emit_empty();
         return;
 
     case SPEC_TYPEDEF:
         emit_typedef(decl);
-        fputc('\n', result);
+        emit_empty();
         return;
     }
 }
@@ -380,43 +427,42 @@ int do_merge(int argc, char** argv)
     result = stdout;
     atexit(cleanup);
 
-    char** last = NULL;
+    char** past_end = NULL;
     while (0 < argc) {
         char* arg = (argc--, *argv++);
         char* val;
         if (!memcmp("-o", arg, 2)) {
-            last = argv-1;
+            past_end = argv-1;
             val = arg[2] ? arg+2 : (argc--, *argv++);
             if (!val) result = NULL;
             else if ('-' != val[0]) result = fopen(val, "wb");
             break;
         }
     }
-    if (!last) last = argv;
+    if (!past_end) past_end = argv;
     if (!result) exitf("Missing result operand or file not writable");
 
-    for (char** it = first; it < last; it++)
-        fprintf(result, "#include \"%s\"\n", *it);
-    fprintf(result, "\n");
+    for (char** it = first; it < past_end; it++) emitln("#include \"%s\"", *it);
+    emit_empty();
 
-    fprintf(result, "static struct adpt_namespace const namespaces[] = {\n");
-    for (char** it = first; it < last; it++) {
+    indented ("static struct adpt_namespace const namespaces[] = {") for (char** it = first; it < past_end; it++) {
         bufsl itns = name_space(*it);
-        // adapter files are expected to start with "a-"
+        // adapter files are expected to start with "a-",
         // tho this is not strictly mandatory I suppose...
+        // this will also catch false positives :/
         if ('a' == itns.ptr[0] && '_' == itns.ptr[1]) {
             itns.ptr+= 2;
             itns.len-= 2;
         }
-        fprintf(result, "    { .name= \"%.*s\"\n", bufmt(itns));
-        fprintf(result, "    , .count= countof(adptns_%.*s)\n", bufmt(itns));
-        fprintf(result, "    , .items= adptns_%.*s },\n", bufmt(itns));
+        emit("{.name= \"%.*s\", .count= countof(adptns_%.*s), .items= adptns_%.*s}", bufmt(itns), bufmt(itns), bufmt(itns));
+        if (past_end == it+1) emit(",");
+        else emitln(",");
     }
-    fprintf(result, "};\n");
-    fprintf(result, "\n");
+    emitln("};");
+    emit_empty();
 
-    fprintf(result, "#define CINTRE_NAMESPACES_DEFINED\n");
-    fprintf(result, "#include \"cintre.c\"\n");
+    emitln("#define CINTRE_NAMESPACES_DEFINED");
+    emitln("#include \"cintre.c\"");
 
     return EXIT_SUCCESS;
 }
@@ -452,7 +498,8 @@ int do_prepare(int argc, char** argv)
     }
 
     if (!result) exitf("Missing result operand or file not writable");
-    fprintf(result, "#include \"adapter.h\"\n\n");
+    emitln("#include \"adapter.h\"");
+    emit_empty();
 
     lini(&ls, file);
 
@@ -487,31 +534,15 @@ int do_prepare(int argc, char** argv)
     }
 
     bufsl thisns = name_space(file);
-    fprintf(result, "static struct adpt_item const adptns_%.*s[] = {\n", bufmt(thisns));
-    for (size_t k = 0; k < seen.funs.len; k++) {
-        fprintf(result, "    { .name= \"%.*s\"\n", bufmt(seen.funs.ptr[k].name));
-        fprintf(result, "    , .type= &%.*s_adapt_type\n", bufmt(seen.funs.ptr[k].name));
-        fprintf(result, "    , .as.function= %.*s_adapt_call\n", bufmt(seen.funs.ptr[k].name));
-        fprintf(result, "    , .kind= ITEM_VALUE },\n");
+    indented ("static struct adpt_item const adptns_%.*s[] = {", bufmt(thisns)) {
+        for (size_t k = 0; k < seen.funs.len; k++) emitln("{.name= \"%.*s\", .type= &%.*s_adapt_type, .kind= ITEM_VALUE, .as.function= %.*s_adapt_call}", bufmt(seen.funs.ptr[k].name), bufmt(seen.funs.ptr[k].name), bufmt(seen.funs.ptr[k].name));
+        for (size_t k = 0; k < seen.tags.len; k++) emitln("{.name= \"@%.*s\", .type= &%.*s_adapt_tag_type, .kind= ITEM_TYPEDEF},", bufmt(seen.tags.ptr[k].name), bufmt(seen.tags.ptr[k].name));
+        for (size_t k = 0; k < seen.tdfs.len; k++) emitln("{.name= \"%.*s\", .type= &%.*s_adapt_type, .kind= ITEM_TYPEDEF},", bufmt(seen.tdfs.ptr[k].name), bufmt(seen.tdfs.ptr[k].name));
+        for (size_t k = 0; k < seen.objs.len; k++) emitln("{.name= \"%.*s\", .type= &%.*s_adapt_type, .kind= ITEM_VARIABLE, .as.object= &%.*s},", bufmt(seen.objs.ptr[k].name), bufmt(seen.objs.ptr[k].name), bufmt(seen.objs.ptr[k].name));
+        size_t const count = seen.funs.len+seen.tags.len+seen.tdfs.len+seen.objs.len;
+        emit("// exporting %zu name%s", count, 1 == count ? "" : "s");
     }
-    for (size_t k = 0; k < seen.tags.len; k++) {
-        fprintf(result, "    { .name= \"@%.*s\"\n", bufmt(seen.tags.ptr[k].name));
-        fprintf(result, "    , .type= &%.*s_adapt_tag_type\n", bufmt(seen.tags.ptr[k].name));
-        fprintf(result, "    , .kind= ITEM_TYPEDEF },\n");
-    }
-    for (size_t k = 0; k < seen.tdfs.len; k++) {
-        fprintf(result, "    { .name= \"%.*s\"\n", bufmt(seen.tdfs.ptr[k].name));
-        fprintf(result, "    , .type= &%.*s_adapt_type\n", bufmt(seen.tdfs.ptr[k].name));
-        fprintf(result, "    , .kind= ITEM_TYPEDEF },\n");
-    }
-    for (size_t k = 0; k < seen.objs.len; k++) {
-        errdie("NIY (other kinds of static variable declaration)");
-        fprintf(result, "    { .name= \"%.*s\"\n", bufmt(seen.objs.ptr[k].name));
-        fprintf(result, "    , .type= &%.*s_adapt_type\n", bufmt(seen.objs.ptr[k].name));
-        fprintf(result, "    , .as.object= &%.*s\n", bufmt(seen.objs.ptr[k].name));
-        fprintf(result, "    , .kind= ITEM_VARIABLE },\n");
-    }
-    fprintf(result, "};\n");
+    emitln("};");
 
     return EXIT_SUCCESS;
 }
